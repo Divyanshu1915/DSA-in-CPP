acfdd1d8bed3361cdff6b92e592f2e4bed155116
Divyanshu1915 <divyanshu19singh@gmail.com>
2026-06-26 20:50:21 +0530

    Add files via upload

---
+++ returnvalueFUNC.h
@@ -0,0 +1,12 @@
+//
+// Created by Nikita Singh on 03/06/26.
+//
+
+#ifndef DSA_IN_C___RETURNVALUEFUNC_H
+#define DSA_IN_C___RETURNVALUEFUNC_H
+
+
+int FUNC(int x,int y);
+
+
+#endif //DSA_IN_C___RETURNVALUEFUNC_H

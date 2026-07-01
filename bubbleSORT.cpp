d07ff72e3c30b2b5447295c38cb4451fb3b2746a
Divyanshu1915 <divyanshu19singh@gmail.com>
2026-07-01 21:36:06 +0530

    Add files via upload

---
+++ bubbleSORT.cpp
@@ -0,0 +1,53 @@
+//
+// Created by Nikita Singh on 03/06/26.
+//
+
+#include "bubbleSORT.h"
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int SORT(int len) {
+    //for input in array
+    std::vector<int> arr1(len);
+
+    int i;
+    for (i=0;i<len;i++) {
+        std:: cout<<"Enter the vakue at index "<<i<<"array:";
+        std::cin>>arr1[i];
+    }
+
+
+    //for the show of the array
+
+    for(i=0;i<len;i++){
+        cout<<arr1[i]<<"  ";
+    }
+    cout<<endl;
+
+
+    int j;
+    int t;
+    for (i=0;i<len-1;i++) {
+        for (j=0;j<len-i-1;j++) {
+            if (arr1[j]>arr1[j+1]) {
+
+                t=arr1[j];
+                arr1[j]=arr1[j+1];
+                arr1[j+1]=t;
+
+            }
+            cout<<arr1[j]<<" ";
+
+        }
+        cout<<endl;
+    }
+    //print sorted array
+    cout<<"sorted array"<<endl;
+    for (i=0;i<len;i++) {
+
+        cout<<arr1[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
---
+++ bubbleSORT.h
@@ -0,0 +1,13 @@
+//
+// Created by Nikita Singh on 03/06/26.
+//
+
+#ifndef DSA_IN_C___BUBBLESORT_H
+#define DSA_IN_C___BUBBLESORT_H
+
+
+class bubbleSORT {
+};
+
+
+#endif //DSA_IN_C___BUBBLESORT_H

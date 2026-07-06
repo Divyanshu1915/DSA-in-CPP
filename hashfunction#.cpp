10b170c147eb290f6c2dd012e26136e81f4587fb
Divyanshu1915 <divyanshu19singh@gmail.com>
2026-07-06 22:49:52 +0530

    Refactor hash set implementation and functions

--- hashfunction#.cpp
+++ hashfunction#.cpp
@@ -6,73 +6,72 @@ using namespace std;
 
 // Hash Set using separate chaining
 vector<vector<string>> myHashSet = {
-    {""},
+    {},
     {"Jones"},
-    {""},
+    {},
     {"Lisa"},
-    {""},
+    {},
     {"Bob"},
-    {""},
+    {},
     {"Siri"},
     {"Pete"},
-    {""}
+    {}
 };
 
-int hashFunction(string name){
-    int a=0;
-    for (char ch : name){
+int hashFunction(string name) {
+    int a = 0;
+    for (char ch : name) {
         a += ch;
-
     }
-    return a %
+    return a % myHashSet.size();
 }
-//ADD a value to the hash Set
-void add(string name){
-    int index=hashFunction(name);
-    vector<string>& bucket=myHashSet[index];
-    //check if value already exist
-    for(string item : bucket){
-        if(item== name)
+
+// Add a value to the hash set
+void add(string name) {
+    int index = hashFunction(name);
+    vector<string>& bucket = myHashSet[index];
+
+    // Check if already exists
+    for (string item : bucket) {
+        if (item == name)
             return;
     }
-    bucket.push_back(name);
 
+    bucket.push_back(name);
 }
 
-//check if value exist
-int Contains(string name){
-    int index=hashFunction(name);
-    vector<string>& bucket=myHashSet[index];
+// Check if value exists
+bool Contains(string name) {
+    int index = hashFunction(name);
+    vector<string>& bucket = myHashSet[index];
 
-    for(string item : bucket){
-        if(item== name){
-            return true
+    for (string item : bucket) {
+        if (item == name) {
+            return true;
         }
     }
+
     return false;
 }
 
-int main(){
+int main() {
     add("Stuart");
 
-    //print hash set
-    cout<<"HAsh Set:\n";
-    for(int i=0;i<myHashSet.size();i++){
-        cout<<i<<":";
-        for (string item : myHashSet[i]){
-            cout<<item<<" ";
+    cout << "Hash Set:\n";
+    for (int i = 0; i < myHashSet.size(); i++) {
+        cout << i << ": ";
+        for (string item : myHashSet[i]) {
+            cout << item << " ";
         }
-        cout<<end;
-
+        cout << endl;
     }
 
-    cout<<"Contains Stuart\n";
-    if(contains("Stuart")){
-        cout<<"true";
-    }
-    else{
-        cout<<"false";
-    }
+    cout << "\nContains Stuart: ";
+
+    if (Contains("Stuart"))
+        cout << "true";
+    else
+        cout << "false";
+
     return 0;
-
 }

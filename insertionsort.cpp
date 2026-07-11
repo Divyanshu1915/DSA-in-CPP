0c8b34e8087709277acfdd805c43482d012ec095
Divyanshu1915 <divyanshu19singh@gmail.com>
2026-07-11 21:04:16 +0530

    Refactor insertion sort implementation in SORT3
    Refactor SORT3 function for clarity and correctness. Improved element shifting and added proper output formatting.

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -1,26 +1,30 @@
-//
-// Created by Nikita Singh on 06/06/26.
-//
-
 #include "insertionsort.h"
-#include<iostream>
+#include <iostream>
 using namespace std;
-void SORT3(int len, int arr[]) {
 
-    for (int i=1;i<len;i++) {
-        int curr=arr[i];
-        for (int j=i;j>=0;j--) {
-            if (arr[j]>curr) {
-                arr[j]=arr[j-1];
-                arr[j-1]=arr[j];
+void SORT3(int len, int arr[])
+{
+    for (int i = 1; i < len; i++)
+    {
+        int curr = arr[i];
+        int j = i - 1;
 
-            }
-            cout<<arr[j]<<"  ";
-
+        // Shift elements greater than curr to the right
+        while (j >= 0 && arr[j] > curr)
+        {
+            arr[j + 1] = arr[j];
+            j--;
         }
 
+        // Insert current element
+        arr[j + 1] = curr;
     }
-    for(int i=0;i<len;i++) {
-        cout<<arr[i]<<" ";
+
+    // Print sorted array
+    cout << "Sorted Array: ";
+    for (int i = 0; i < len; i++)
+    {
+        cout << arr[i] << " ";
     }
+    cout << endl;
 }
